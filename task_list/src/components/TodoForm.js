371d7980b9363107f116eac0ea6294ddb2a35fe9
Ana Bindiu <anabindiu11@gmail.com>
2022-03-25 14:39:48 -0600

    Edits to to do list

--- task_list/src/components/TodoForm.js
+++ task_list/src/components/TodoForm.js
@@ -2,25 +2,32 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : '');
+  const [name_of_Task, setName] = useState(props.edit ? props.edit.value : '');
+  const [description, setDescription] = useState(props.edit ? props.edit.value : '');
+  const [location, setLocation] = useState(props.edit ? props.edit.value : '');
+  const [deadline, setDeadline] = useState(props.edit ? props.edit.value : '');
 
   const inputRef = useRef(null);
+  const nameRef = useRef(null);
+  const descrRref = useRef(null);
+  const locationRef = useRef(null);
+  const deadlineRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
   });
 
-  const handleChange = e => {
-    setInput(e.target.value);
-  };
-
   const handleSubmit = e => {
     e.preventDefault();
-
+    const input_variables = {
+      input, name_of_Task
+    }
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input
     });
     setInput('');
+    setName('');
   };
 
   return (
@@ -30,7 +37,7 @@ function TodoForm(props) {
           <input
             placeholder='Edit task information'
             value={input}
-            onChange={handleChange}
+            onChange={(e) => setInput(e.target.value)}
             name='text'
             ref={inputRef}
             className='todo-input edit'
@@ -44,10 +51,47 @@ function TodoForm(props) {
           <input
             placeholder='Add task'
             value={input}
-            onChange={handleChange}
+            onChange={(e) => setInput(e.target.value)}
             name='text'
             className='todo-input'
             ref={inputRef}
+          />
+
+          <div></div>
+          <input
+            placeholder='Title'
+            value={name_of_Task}
+            onChange={(e) => setName(e.target.value)}
+            name='title'
+            className='todo-input'
+            ref={nameRef}
+          />
+          <div></div>
+          <input
+            placeholder='Description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            name='description'
+            className='todo-input'
+            ref={descrRref}
+          />
+          <div></div>
+          <input
+            placeholder='Location'
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            name='location'
+            className='todo-input'
+            ref={locationRef}
+          />
+          <div></div>
+          <input
+            placeholder='Deadline'
+            value={deadline}
+            onChange={(e) => setDeadline(e.target.value)}
+            name='deadline'
+            className='todo-input'
+            ref={deadlineRef}
           />
           <button onClick={handleSubmit} className='todo-button'>
             Add
