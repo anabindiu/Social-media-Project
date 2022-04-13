ce575ee6e7e08d890d95d3a388c8919e8d0d181e
Ana Bindiu <anabindiu11@gmail.com>
2022-04-13 13:34:31 -0600

    modifications

--- api/services/db.js
+++ api/services/db.js
@@ -4,10 +4,16 @@ const config = require('../config');
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
   const [results, ] = await connection.execute(sql, params);
-
+  connection.end();
   return results;
 }
 
 module.exports = {
   query
 }
+
+
+
+
+
+
